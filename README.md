4121bb9bf62c1843e30ac4d5e367dcc418516abe
Sparsh Bansal <sparshbansal787@gmail.com>
2024-04-11 14:48:27 +0530

    main

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# feedback-collection-system
- 
+# feedback-system
+Customer Feedback System using MERN(MongoDb+Express.js+React.js+Node.js), Redux, JWT, Passport.js and Nodemailer
---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mongoURI: "Your Mongo URI here",
+    secretOrKey: "secret"
+    
+    //Mail Configuration
+    host: "smtp.gmail.com", //for gmail user
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    user: "Email Username",//gmail id for gmail user
+    pass: "Email Password"//gmail password for gmail user
+  };
